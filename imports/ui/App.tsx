1cead59b77f260e71d6214f907b8f2378766be55
kazuki <kaz.tokyo.tech20@gmail.com>
2022-02-24 08:55:52 +0900

    add Collections

--- imports/ui/App.tsx
+++ imports/ui/App.tsx
@@ -1,24 +1,34 @@
+import { useTracker } from "meteor/react-meteor-data";
 import React from "react";
 import { Task } from "../../interfaces/task";
+import { TasksCollection } from "../api/TasksCollection";
 import { Hello } from "./Hello";
 import { Info } from "./Info";
 import { TaskExpress } from "./TaskExpress";
 
-const tasks: Task[] = [
-	{ _id: 1, text: "This is task 1" },
-	{ _id: 2, text: "This is task 2" },
-	{ _id: 3, text: "This is task 3" },
-];
+// const tasks: Task[] = [
+// 	{ _id: 1, text: "This is task 1" },
+// 	{ _id: 2, text: "This is task 2" },
+// 	{ _id: 3, text: "This is task 3" },
+// ];
 
-export const App = () => (
-	<div>
-		<h1>Welcome to Meteor!</h1>
-		<Hello />
-		<Info />
-		<ul>
-			{tasks.map((task) => (
-				<TaskExpress _id={task._id} text={task.text} />
-			))}
-		</ul>
-	</div>
-);
+export const App = () => {
+
+	const tasks: Task[] = useTracker(() => {
+		TasksCollection.find({}).fetch();
+	});
+  // void かもしれないので、処理を加える必要がある
+  
+	return (
+		<div>
+			<h1>Welcome to Meteor!</h1>
+			<Hello />
+			<Info />
+			<ul>
+				{tasks.map((task) => (
+					<TaskExpress _id={task._id} text={task.text} />
+				))}
+			</ul>
+		</div>
+	);
+};
--- server/main.ts
+++ server/main.ts
@@ -1,9 +1,12 @@
 import { Meteor } from "meteor/meteor";
 import { LinksCollection } from "/imports/api/links";
+import { TasksCollection } from "/imports/api/TasksCollection";
 
 function insertLink(title: string, url: string) {
 	LinksCollection.insert({ title, url, createdAt: new Date() });
 }
+const insertTask = (taskText: string) =>
+	TasksCollection.insert(taskText);
 
 Meteor.startup(() => {
 	// If the Links collection is empty, add some data.
@@ -18,5 +21,17 @@ Meteor.startup(() => {
 		insertLink("Read the Docs", "https://docs.meteor.com");
 
 		insertLink("Discussions", "https://forums.meteor.com");
+	}
+
+	if (TasksCollection.find().count() === 0) {
+		[
+			"First Task",
+			"Second Task",
+			"Third Task",
+			"Fourth Task",
+			"Fifth Task",
+		].forEach((taskText: string) => {
+			insertTask(taskText);
+		});
 	}
 });
